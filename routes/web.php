636901a78586a3e5351ead01cb6642685649e3fc
Summer <csy13808796047@gmail.com>
2019-09-24 10:16:04 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 //通知列表
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+//无权限访问
+Route::get('permission-denied', 'pagesController@permissionDenied')->name('permission-denied');
