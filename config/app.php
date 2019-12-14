6b77d560b19c3d01169856c949dd6dd8c468ef22
Summer <csy13808796047@gmail.com>
2019-12-14 11:40:19 +0800

    短信配置

--- config/app.php
+++ config/app.php
@@ -175,7 +175,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        //发送短信的provider
+        App\Providers\EasySmsServiceProvider::class,
     ],
 
     /*
