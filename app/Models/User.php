d9454d782c83a5166666a950da80e2548ac02102
Summer <csy13808796047@gmail.com>
2019-12-28 17:53:26 +0800

    使用JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,21 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail, JWTSubject
 {
     use Notifiable, HasRoles, ActiveUserHelper, LastActiveAtHelper;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     //回复通知数
     public function TopicNotify($instance)
