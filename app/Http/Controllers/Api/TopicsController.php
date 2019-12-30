b3f967cf3ef409b33d4900da5d4aed9d3b561687
Summer <csy13808796047@gmail.com>
2019-12-30 17:13:45 +0800

    话题列表

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -5,10 +5,33 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\TopicRequest;
 use App\Http\Resources\TopicResource;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class TopicsController extends Controller
 {
+    public function index(Request $request, Topic $topic)
+    {
+//        $query = $topic->query();
+//        if ($categoryId = $request->category_id) {
+//            $query->where('category_id', $categoryId);
+//        }
+//        $topics = $query
+//            ->with('user', 'category')
+//            ->withOrder($request->order)
+//            ->paginate();
+        $topics = QueryBuilder::for(Topic::class)
+            ->allowedIncludes('user', 'category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ])->paginate();
+        return TopicResource::collection($topics);
+    }
+
     public function store(TopicRequest $request, Topic $topic)
     {
         $topic->fill($request->all());
@@ -29,5 +52,20 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
         $topic->delete();
         return response(null, 204);
+    }
+
+    //查询某个人发布的话题
+    public function userIndex(Request $request, User $user)
+    {
+        $query = $user->topics()->getQuery();
+        $topics = QueryBuilder::for($query)
+            ->allowedIncludes('user', 'category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ])
+            ->paginate();
+        return TopicResource::collection($topics);
     }
 }
