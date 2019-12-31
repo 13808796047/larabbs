b17eb571bceee5da24e52890deeb759188327364
Summer <csy13808796047@gmail.com>
2019-12-31 15:18:39 +0800

    本地化

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function errorResponse($statusCode, $message = null, $code = 0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
